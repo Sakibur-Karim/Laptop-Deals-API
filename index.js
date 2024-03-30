c9d905cd086a59b135d2664957bd74a503edead5
Sakibur-Karim <sakiburkarim@gmail.com>
2024-03-30 11:23:10 -0400

    Update

--- index.js
+++ index.js
@@ -1,45 +1,34 @@
-const PORT = process.env.PORT || 3000;
-const axios = require('axios');
-const express = require('express');
-const cheerio = require('cheerio');
-const app = express();
-const sources = [];
+const PORT = process.env.PORT || 3000
+const axios = require('axios')
+const express = require('express')
+const cheerio = require('cheerio')
+const app = express()
+const sources = []
 
 const urls = [
   {
     address: 'https://old.reddit.com/r/LaptopDeals/new/',
   },
-];
+]
 
-// Create an array of promises for each request
-const requests = urls.map((url) => axios.get(url.address));
+urls.forEach((url) => {
+  axios.get(url.address).then((response) => {
+    const html = response.data
+    const $ = cheerio.load(html)
 
-// Wait for all requests to complete
-Promise.all(requests)
-  .then((responses) => {
-    responses.forEach((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-
-      $('a:contains("$")', html).each(function () {
-        const title = $(this).text();
-        const url = $(this).attr('href');
-        sources.push({
-          title,
-          url,
-        });
-      });
-    });
-
-    // Start the server once all requests are processed
-    app.listen(PORT, () => {
-      console.log(`Server opened at PORT ${PORT}`);
-    });
+    $('a:contains("$")', html).each(function () {
+      const title = $(this).text()
+      const url = $(this).attr('href')
+      sources.push({
+        title,
+        url,
+      })
+    })
   })
-  .catch((error) => {
-    console.error('Error fetching data:', error.message);
-  });
+})
 
 app.get('/', function (req, res) {
-  res.json(sources);
-});
+  res.json(sources)
+})
+
+app.listen(PORT, () => console.log(`Server opened at PORT ${PORT}`))
